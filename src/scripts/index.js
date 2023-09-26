3f4880315ccf6e04d6ab45c2786171ab42ab9f92
Juan Prasetyo <juanprasetyo000@gmail.com>
2023-09-26 18:55:33 +0700

    import file main.scss & bootstrap icon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,3 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
-
-console.log('Hello Coders! :)');
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../styles/main.scss';
