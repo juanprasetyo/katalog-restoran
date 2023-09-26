d6d4577b64163e40f7461f4994b013a196ee3895
Juan Prasetyo <juanprasetyo000@gmail.com>
2023-09-26 19:15:52 +0700

    menampilkan list restoran

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../styles/main.scss';
+import './components/restaurant-list';
 
 const navbarToggleElement = document.querySelector('.navbar-toggle');
 const navbarNavElement = document.querySelector('.navbar-nav');
@@ -15,3 +16,11 @@ mainElement.addEventListener('click', (event) => {
   event.stopPropagation();
   navbarNavElement.classList.remove('show');
 })
+
+const data = require('../public/data/DATA.json');
+const restaurants = data.restaurants;
+
+
+const restaurantListElement = document.createElement('restaurant-list');
+restaurantListElement.restaurants = restaurants;
+mainElement.appendChild(restaurantListElement);
