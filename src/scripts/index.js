6c38170fe088ee3820c231f097fc485249fd1446
Juan Prasetyo <juanprasetyo000@gmail.com>
2023-09-26 18:59:51 +0700

    buat navbar bisa di show atau hide di tampilan mobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,17 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../styles/main.scss';
+
+const navbarToggleElement = document.querySelector('.navbar-toggle');
+const navbarNavElement = document.querySelector('.navbar-nav');
+const mainElement = document.querySelector('main');
+
+navbarToggleElement.addEventListener('click', (event) => {
+  event.stopPropagation();
+  navbarNavElement.classList.toggle('show');
+})
+
+mainElement.addEventListener('click', (event) => {
+  event.stopPropagation();
+  navbarNavElement.classList.remove('show');
+})
